cbf3ccb8a454572ac076715f9e631498b0d5cdff
Ben Wheatley <ben@kitsunesoftware.com>
2018-04-12 17:26:01 +0100

    Secure RNG, closing issue #1 noted on HackerNews comments

--- HighEntropyPassword.py
+++ HighEntropyPassword.py
@@ -10,12 +10,14 @@ f.close()
 
 numOfWords = len(allWords)
 
-wordIndex1 = random.randint(0, numOfWords-1)
-wordIndex2 = random.randint(0, numOfWords-1)
-wordIndex3 = random.randint(0, numOfWords-1)
+secureRNG = random.SystemRandom()
 
-separator1 = str(random.randint(0, 1000))
-separator2 = str(random.randint(0, 1000))
+wordIndex1 = secureRNG.randint(0, numOfWords-1)
+wordIndex2 = secureRNG.randint(0, numOfWords-1)
+wordIndex3 = secureRNG.randint(0, numOfWords-1)
+
+separator1 = str(secureRNG.randint(0, 1000))
+separator2 = str(secureRNG.randint(0, 1000))
 
 print("\nCorrect Horse Battery Staple (73 bits entropy, if random number generator is correct and your dictionary is the same size as mine):")
 print( allWords[wordIndex1].strip() + separator1 + allWords[wordIndex2].strip() + separator2 + allWords[wordIndex3].strip() )
@@ -28,7 +30,7 @@ numChars = len(chars)
 randomString = ""
 length = 12
 for x in range(0, length):
-	charIndex = random.randint(0, numChars-1)
+	charIndex = secureRNG.randint(0, numChars-1)
 	randomString = randomString + chars[charIndex]
 
 entropy = math.log(chars.__len__() ** length, 2) # Can't use math.log2(...) because that's python 3
